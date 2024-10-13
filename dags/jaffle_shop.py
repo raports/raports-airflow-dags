1337a883bdf568079c6c0bbf14c32aeaa6746bb1
Ramis Khasianov <ramis.khasianov@gmail.com>
2024-10-13 22:08:58 +0500

    test another path

--- dags/jaffle_shop.py
+++ dags/jaffle_shop.py
@@ -10,7 +10,7 @@ import os
 
 DAG_ID = "jaffle_shop_pipeline"
 CONNECTION_ID = "dwh"
-DBT_PROJECT_PATH = f"{os.environ['PWD']}/dbt/jaffle-shop"
+DBT_PROJECT_PATH = f"{os.environ['AIRFLOW_HOME']}/dags/repo/dags/dbt/jaffle-shop"
 DBT_EXECUTABLE_PATH = f"{os.environ['AIRFLOW_HOME']}/dbt_venv/bin/dbt"
 
 
